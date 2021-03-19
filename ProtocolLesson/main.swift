80403386d9590e7f8221a24a9d1d94894a9a1f68
Argen <n.uulu.a@mail.ru>
2021-03-20 00:38:26 +0900

    Homework 5 changed

--- ProtocolLesson/main.swift
+++ ProtocolLesson/main.swift
@@ -8,13 +8,64 @@
 import Foundation
 
 protocol CarProtocol {
-    
+
     var speed: Double { get set }
-    
+
     func move ()
 }
 
-extension CarProtocol {
+
+protocol DoorStateProtocol {
+
+    var doorState: String { get set }
+
+    func door()
+}
+
+
+protocol EngineState: class {
+
+    var enginState: String { get set }
+
+    func start ()
+}
+
+
+class SporCar: CarProtocol, DoorStateProtocol, EngineState {
+
+    let year: Int
+    let km: Double
+
+    var speed: Double = 260
+
+    var doorState: String = "закрыто"
+
+    var enginState: String = "запущен"
+
+    init(year: Int, km: Double) {
+        self.year = year
+        self.km = km
+    }
+
+}
+
+extension SporCar: CustomStringConvertible {
+    
+    var description: String {
+        return "Вид SportCar"
+    }
+    func infoCar(_ car: SporCar) {
+        print("Год: \(year)")
+        print("Км: \(km)")
+    }
+    
+    func start() {
+        print("Двигатель \(enginState)")
+    }
+    func door() {
+        print("Дверь \(doorState)")
+
+    }
     
     func move() {
         print("Я еду со скоростью \(speed)")
@@ -22,93 +73,38 @@ extension CarProtocol {
 }
 
 
-protocol DoorStateProtocol {
-    
-    var doorState: String { get set }
-    
-    func door()
-}
-
-extension DoorStateProtocol {
-    
-    func door() {
-        print("Дверь \(doorState)")
-        
-    }
-}
-
-
-protocol EngineState: class {
-    
-    var enginState: String { get set }
-    
-    func start ()
-}
-
-extension EngineState {
-    
-    func start() {
-        print("Двигатель \(enginState)")
-    }
-}
-
-class SporCar: CarProtocol, DoorStateProtocol, EngineState {
-    
-    let year: Int
-    let km: Double
-    
-    var speed: Double = 260
-    
-    var doorState: String = "закрыто"
-    
-    var enginState: String = "запущен"
-    
-    init(year: Int, km: Double) {
-        self.year = year
-        self.km = km
-    }
-    
-    func infoCar(car: SporCar) {
-        print("Год: \(year)")
-        print("Км: \(km)")
-    }
-    
-}
-
 let sportCar = SporCar(year: 2020, km: 234)
 
-sportCar.infoCar(car: sportCar)
+sportCar.infoCar(sportCar)
 sportCar.start()
 sportCar.move()
 sportCar.door()
-  
-extension SporCar: CustomStringConvertible {
-    var description: String {
-        return "Вид SportCar"
-    }
-}
 
-print(sportCar.description)
+print(sportCar)
 
 print("-------------------------------")
 
 class TrackCar: CarProtocol, DoorStateProtocol, EngineState {
-    
+
     let markTrack: String
     let seats: Int
     var luggage: String
-    
+
     var speed: Double = 180
-    
+
     var doorState: String = "открыто"
-    
+
     var enginState: String = "запущен"
-    
+
     init(markTrack: String, seats: Int, luggage: String) {
         self.markTrack = markTrack
         self.seats = seats
         self.luggage = luggage
     }
+
+}
+
+extension TrackCar: CustomStringConvertible {
     
     func infoTrack() {
         print("\(markTrack)")
@@ -116,6 +112,21 @@ class TrackCar: CarProtocol, DoorStateProtocol, EngineState {
         print("Багаж \(luggage)")
     }
     
+    var description: String {
+        return "Вид грузовой автомобиль"
+    }
+    
+    func start() {
+        print("Двигатель \(enginState)")
+    }
+    func door() {
+        print("Дверь \(doorState)")
+
+    }
+    
+    func move() {
+        print("Я еду со скоростью \(speed)")
+    }
 }
 
 let trackCar = TrackCar(markTrack: "Volvo", seats: 4, luggage: "пустой")
@@ -124,11 +135,4 @@ trackCar.door()
 trackCar.start()
 trackCar.move()
 
-extension TrackCar: CustomStringConvertible {
-    var description: String {
-        return "Вид грузовой автомобиль"
-    }
-}
-print(trackCar.description)
-
-
+print(trackCar)
